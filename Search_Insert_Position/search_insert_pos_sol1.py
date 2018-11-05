6aa1337207eb7a820b5925be4297d59af92d3dd4
ujjwaljoshi93 <ujjwaljoshi93@gmail.com>
2018-11-05 17:08:48 -0500

    search_insert_positon-sol1

---
+++ Search_Insert_Position/search_insert_pos_sol1.py
@@ -0,0 +1,11 @@
+class Solution:
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        for i in nums:
+            if i >= target:
+                return nums.index(i)
+        return len(nums)
