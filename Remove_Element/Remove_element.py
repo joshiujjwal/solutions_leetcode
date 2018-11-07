3941ad5e00cc51b1e5706e0c96ed6b70acb660d8
ujjwaljoshi93 <ujjwaljoshi93@gmail.com>
2018-11-07 01:17:09 -0500

    Remove Element Sol1

---
+++ Remove_Element/Remove_element.py
@@ -0,0 +1,18 @@
+class Solution:
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        i = 0
+        while i<len(nums):
+            if nums[i] == val:
+                nums.remove(val)
+                i = 0
+            else:
+                i+=1
+            
+        return len(nums)
+        
+        
