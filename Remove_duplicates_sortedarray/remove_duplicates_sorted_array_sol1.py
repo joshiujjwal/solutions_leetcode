38fbf0af485a889a0ac8a904ccb919e19bb042fa
ujjwaljoshi93 <ujjwaljoshi93@gmail.com>
2018-11-08 19:36:04 -0500

    remove_duplicates_sorted_array_sol1

---
+++ Remove_duplicates_sortedarray/remove_duplicates_sorted_array_sol1.py
@@ -0,0 +1,15 @@
+class Solution:
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        i=0
+        while(i<len(nums)-1):
+            if (nums[i]==nums[i+1]):
+                nums.remove(nums[i])
+                i-=1
+            i+=1
+        
+        return len(nums)
+        
